1a02ace4734a579868dd0e8039be32c10ca0db0f
angelampatarroyo <43211578+angelampatarroyo@users.noreply.github.com>
2023-12-15 10:49:14 -0500

    incio proyecto estudio rest

---
+++ src/test/java/com/springboot/rest/RestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jdbc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
